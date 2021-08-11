1c2ef45c6448dcd3c1e53fc63b122fc337eed9ad
MuriloChaves <murilochaves@icloud.com>
2021-08-11 09:12:52 -0300

    update

--- beautiful-soup/__main__.py
+++ beautiful-soup/__main__.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
+def main_python_academy():
     '''
     Função principal que, realiza todo o escopo de código da conversão.
     '''
@@ -24,5 +24,33 @@ def main():
         print(titulo.get_text().strip())
 
 
+def main_fundamentus():
+    '''
+    Função principal que, realiza todo o escopo de código da conversão.
+    '''
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # Busca o código HTML da página que quer analisar
+    pagina = requests.get('https://fundamentus.com.br/fii_resultado.php', headers=headers)
+
+    # Instancia o BeautifulSoup, passando o HTML no construtor e o parser
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+
+    # Ache o dado que quer na página (passo 2)
+    dados = soup.find('table', {'id': 'tabelaResultado'}).find('tbody').findAll('tr')
+
+    # Agora e com você! ;)
+    for fundo in dados:
+        dados_fundo = fundo.find_all('td')
+        print(
+            f'[{dados_fundo[0].text}]\n'
+            f'\tCotação: {dados_fundo[2].text}\n'
+            f'\tSetor: {dados_fundo[1].text}\n'
+            f'\tDY %: {dados_fundo[4].text}\n'
+            f'\tP/VPA: {dados_fundo[5].text}\n'
+        )
+    
+
 if __name__ == '__main__':
-    main()
+    main_fundamentus()
